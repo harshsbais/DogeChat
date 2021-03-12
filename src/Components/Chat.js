ca56491bdb3dc64f4528df791ece28d4fb71e504
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-12 14:09:06 +0530

    chat box boilerplate

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { read_cookie, delete_cookie } from 'sfcookies';
+import { read_cookie } from 'sfcookies';
 import UserInfo from './UserInfo'
+import { Button } from 'react-bootstrap'
+import './Chat.scss'
 function Chat() {
     const [modalShow, setModalShow] = useState(true);
     useEffect(() => {
@@ -11,13 +13,21 @@ function Chat() {
         }
     }, [])
     return (
-        <div>
+        <div className='chatBox'>
             <UserInfo
                 show={modalShow}
                 onHide={() => setModalShow(false)}
             />
-            Chat Box
-        </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                <p className='mx-auto pt-1' style={{ fontSize: '40px', fontFamily: "'Redressed', cursive" }}>Global Chat</p>
+            </nav>
+
+
+            <div className='footer'>
+                <input className="mt-4" style={{ width: '70%' }} />
+                <Button className="ml-4"><i className="fa fa-arrow-right"></i></Button>
+            </div>
+        </div >
     )
 }
 
