97d535da516dcebda1ec150b0883ef228dad39f7
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-12 18:00:56 +0530

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyAyUNVeeOlsG8Cd75HEkD1NzlY0-VZbwLg",
+    authDomain: "global-chat-55981.firebaseapp.com",
+    projectId: "global-chat-55981",
+    storageBucket: "global-chat-55981.appspot.com",
+    messagingSenderId: "647779785901",
+    appId: "1:647779785901:web:27faec5d45ee12a67c3a24"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export default db;
