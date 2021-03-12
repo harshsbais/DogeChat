4a4724926c3e7fe3d2f83690df122eb7bb68b2cc
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-13 00:06:52 +0530

    Fix css bug

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { read_cookie } from 'sfcookies';
 import UserInfo from './UserInfo'
-import { Form } from 'react-bootstrap'
-import './Chat.scss'
+import { Form, Row } from 'react-bootstrap'
+import './Chat.css'
 function Chat() {
     const [modalShow, setModalShow] = useState(true);
     const [message, setMessage] = useState({});
@@ -22,42 +22,72 @@ function Chat() {
         setMessage({ ...message, [e.target.name]: e.target.value });
     }
     return (
-        <div className='chatBox'>
-            <UserInfo
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-            <nav className="navbar">
-                <p className='mx-auto pt-1 navbar-inner' style={{ fontSize: '40px', fontFamily: "'Redressed', cursive" }}>Global Chat</p>
-            </nav>
-            <div className="user-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-            <div className="my-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-            <div className="user-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-            <div className="user-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-            <div className="user-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-            <div className="user-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-            <div className="my-msg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
+        <>
+            <div className='chatBox'>
+                <UserInfo
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                />
+                <nav className="navbar">
+                    <p className='mx-auto pt-1 navbar-inner' style={{ fontSize: '40px', fontFamily: "'Redressed', cursive" }}>Global Chat</p>
+                </nav>
+                <Row style={{ width: '100%' }}>
+                    <div className="user-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row>
+                <Row style={{ width: '100%' }}>
+                    <div className="my-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row>
+                <Row style={{ width: '100%' }}>
+                    <div className="user-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row >
+                <Row style={{ width: '100%' }}>
+                    <div className="user-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row>
+                <Row style={{ width: '100%' }}>
+                    <div className="user-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row>
+                <Row style={{ width: '100%' }}>
+                    <div className="user-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row>
+                <Row style={{ width: '100%' }}>
+                    <div className="my-msg">
+                        <p style={{ color: 'orange' }}>Harsh</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p style={{ color: 'orange' }}>3 days ago</p>
+                    </div>
+                </Row>
+            </div >
             <div className='footer'>
                 <Form onSubmit={handleSubmit}>
                     <input className="mt-4" value={message.content} name='content' onChange={handleChange} style={{ width: '70%' }} />
                     <button className="ml-4" style={{ backgroundColor: 'black', color: 'white', border: 'none' }} type="submit"><i className="fa fa-arrow-right"></i></button>
                 </Form>
             </div>
-        </div >
+        </>
     )
 }
 
