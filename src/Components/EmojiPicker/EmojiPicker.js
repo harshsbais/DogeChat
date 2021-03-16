24a93b8e9d22dbafd368d594def4de3e1e3b316f
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-16 23:03:15 +0530

    emoji selection showing in modal

--- src/Components/EmojiPicker/EmojiPicker.js
+++ src/Components/EmojiPicker/EmojiPicker.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Picker from 'emoji-picker-react';
-import { Modal } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap';
+import useForceUpdate from 'use-force-update';
 function EmojiPicker(props) {
+    const forceUpdate = useForceUpdate();
     const onEmojiClick = (event, emojiObject) => {
         let msg = props.message;
         if (!msg.content)
@@ -9,15 +11,18 @@ function EmojiPicker(props) {
         else
             msg.content += emojiObject.emoji;
         props.setMessage(msg);
+        forceUpdate();
     };
     return (
         <div>
+            {console.log('hello')}
             <Modal
                 {...props}
                 aria-labelledby="contained-modal-title-vcenter"
                 size="sm"
                 centered>
                 <Modal.Body>
+                    <p>{props.message.content}</p>
                     <Picker onEmojiClick={onEmojiClick} />
                 </Modal.Body>
             </Modal>
